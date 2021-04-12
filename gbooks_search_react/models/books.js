0802cc72d5891bcaf4c2ab512619c19d56846016
Local Al <alexander.s.woolley@gmail.com>
2021-04-12 14:09:31 -0600

    momngo schema Books

---
+++ gbooks_search_react/models/books.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const booksSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  // update type for image
+  link: { type: String, required: true },
+});
+
+const Books = mongoose.model("Books", booksSchema);
+
+module.exports = Books;
