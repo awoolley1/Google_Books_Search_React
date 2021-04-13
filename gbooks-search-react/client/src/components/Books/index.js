1390acb25e7910d712a8da4b75c707d8afc5b88f
Local Al <alexander.s.woolley@gmail.com>
2021-04-13 15:45:16 -0600

    Books component

--- gbooks-search-react/client/src/components/Books/index.js
+++ gbooks-search-react/client/src/components/Books/index.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
+import API from "../../api/randomUser.js";
+import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
-function Books(props) {
+
+function Books() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    API.getBook()
+      .then((res) => {
+        console.log(res);
+        setData(res.data.results);
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  
   return (
-    <h1>Books here</h1>
+    <Table striped bordered hover variant="dark">
+    <thead>
+<tr>
+ <th>Title</th> 
+ <th>Author</th>
+ <th>Description</th>
+ <th>Image</th>
+ <th>Link</th>
+</tr>
+</thead><tbody>
+
+{data.map((book, index) => (
+  <tr key={index}>
+  
+ <td>{book.name.first}</td>
+ <td>{book.name.last}</td>
+ <td>{book.email}</td>
+ <td><img src={user.picture.thumbnail} alt="headshot"/></td>
+ <td>{book.location.city}</td>
+ </tr>
+ )
+)}
+</tbody>
+</Table>
   );
 }
 
